8ef2db2cf9d930c13c46f100315a1e9e7b16958e
Andrei <crisnicandrei@gmail.com>
2020-10-19 17:44:36 +0300

    Tema 4

---
+++ CrisnicAndrei/L04/StackOverflow/StackOverflow.Domain.Core/Contents/Questions/QuestionsDomain.cs
@@ -0,0 +1,16 @@
+﻿using Primitives.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static StackOverflow.Domain.Schema.Questions.AddQuestionOp.AddQuestionResult;
+using static PortExt;
+using StackOverflow.Domain.Schema.Questions.AddQuestionOp;
+
+namespace StackOverflow.Domain.Core.Contents.Questions
+{
+    public static class QuestionsDomain
+    {
+        public static Port<IAddQuestionResult> AddQuestion(string title, string body, string tags) =>
+            NewPort<AddQuestionCommand, IAddQuestionResult>(new AddQuestionCommand(title, body, tags));
+    }
+}
